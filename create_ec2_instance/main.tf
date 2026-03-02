3264be92640c005b0e898463b741174722896f70
Ubuntu <ubuntu@ip-172-31-41-121.eu-north-1.compute.internal>
2026-03-02 03:45:31 +0000

    added ec2 instance terraform configuration

---
+++ create_ec2_instance/main.tf
@@ -0,0 +1,25 @@
+terraform {
+    required_version = ">= 0.13"
+    
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.0"
+        }
+    }
+}
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_instance" "web" {
+    ami           = "ami-073130f74f5ffb161"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "WebServer"
+    }
+}
+
+
+
